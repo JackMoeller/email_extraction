a3b9c2578c41e046916a53c5784ce6a43539512c
Jack Moeller <jrm2vz@virginia.edu>
2017-03-20 14:25:57 -0400

    initial commit

---
+++ extract.py
@@ -0,0 +1,35 @@
+#Utility for finsing emails in raw text
+#to run use python 2 and pass filename  as command line argument
+
+import sys
+#from optparse import OptionParser
+#import os.path
+import re
+
+regex = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
+                    "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
+                    "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
+
+def file_to_str(filename):
+    """Returns the contents of filename as a string."""
+    with open(filename) as f:
+        return f.read().lower() # Case is lowered to prevent regex mismatches.
+
+def get_emails(s):
+    """Returns an iterator of matched emails found in string s."""
+    # Removing lines that start with '//' because the regular expression
+    # mistakenly matches patterns like 'http://foo@bar.com' as '//foo@bar.com'.
+    return (email[0] for email in re.findall(regex, s) if not email[0].startswith('//'))
+
+
+
+chars = []
+in_file = open(sys.argv[1], 'r')
+emails = get_emails(in_file.read().lower())
+in_file.close()
+
+out_file = open('emails.txt', 'w')
+
+for i in emails:
+    out_file.write(i + '\n')
+out_file.close()
